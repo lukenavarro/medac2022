b6b768988a57c0498d2298f4612995d029fca9b2
lukenavarro <44287208+lukenavarro@users.noreply.github.com>
2023-01-20 17:32:36 +0100

    Actualizacion SQL

--- Servidor/Practica9/conexionSQL.php
+++ Servidor/Practica9/conexionSQL.php
@@ -1,9 +1,31 @@
 <?php
+function establecerConexion($servidor,$usuario,$contrasenia,$baseDatos){
+    $conexion = new mysqli($servidor,$usuario,$contrasenia,$baseDatos) or die("Error al establecer la conexion");
+    return $conexion;
+    
+}
+function obtenerDatos($conexion){
+    $valores = $conexion->query("SELECT * FROM usuarios");
+    echo "<table>";
+    echo "<tr>";
+    echo "<td> ID </td>";
+    echo "<td> NOMBRE </td>";
+    echo "<td> Apellidos </td>";
+    echo "</tr>";
+    while($row = mysqli_fetch_object($valores))
+        echo "<tr>";
+        foreach ($row as $rowValue){
+            echo "<td>$rowValue</td>";
+        }
+        echo "</tr>";
+    echo "</table>";
+    $valores -> free();
+    $conexion -> next_result();
+    $conexion -> close();
 
-function obtenerDatos(){
     
 
 }
-
-
+$conexion = establecerConexion("localhost","root","","practica9servidor");
+obtenerDatos($conexion);
 ?>
