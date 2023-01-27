c0d8c559b4da838939e7f94a4044de3ba7c35982
Francisco Navarro <franjnavarrodejuan@gmail.com>
2023-01-27 17:56:57 +0100

    Subida Practica Typescript

---
+++ Cliente/Practica13/main.ts
@@ -0,0 +1,6 @@
+import { grados } from "./conversionGrados";
+import { conversionGrados } from "./conversionGrados";
+
+function convertirGrados(){
+    let elemento  = document.getElementById("solucion") as HTMLInputElement;
+}
