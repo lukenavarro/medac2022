f001ce3cf0ee323e5da42894a19d325fd2e27433
Francisco Navarro <franjnavarrodejuan@gmail.com>
2023-02-01 21:17:52 +0100

    TypeScript update

--- Cliente/Practica13/conversionGrados.js
+++ Cliente/Practica13/conversionGrados.js
@@ -1,9 +1,12 @@
-export var grados;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.conversionGrados = exports.grados = void 0;
+var grados;
 (function (grados) {
     grados[grados["C"] = 1] = "C";
     grados[grados["F"] = 2] = "F";
-})(grados || (grados = {}));
-export class conversionGrados {
+})(grados = exports.grados || (exports.grados = {}));
+class conversionGrados {
     static convertirGrados(numGrados, tipo = grados.C) {
         if (tipo == grados.C) {
             return ((numGrados * 9) / 5) + 32;
@@ -13,3 +16,4 @@ export class conversionGrados {
         }
     }
 }
+exports.conversionGrados = conversionGrados;
--- Cliente/Practica13/main.js
+++ Cliente/Practica13/main.js
@@ -1,13 +1,15 @@
-import { grados } from "./conversionGrados";
-import { conversionGrados } from "./conversionGrados";
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const conversionGrados_1 = require("./conversionGrados");
+const conversionGrados_2 = require("./conversionGrados");
 function convertirGrados() {
     var _a;
     let solucion = document.getElementById("solucion");
     let valor = parseInt(document.getElementById("grados").value);
     if ((_a = document.getElementById("celsius")) === null || _a === void 0 ? void 0 : _a.ariaChecked) {
-        solucion.innerHTML += conversionGrados.convertirGrados(valor, grados.C);
+        solucion.innerHTML += conversionGrados_2.conversionGrados.convertirGrados(valor, conversionGrados_1.grados.C);
     }
     else {
-        solucion.innerHTML += conversionGrados.convertirGrados(valor, grados.F);
+        solucion.innerHTML += conversionGrados_2.conversionGrados.convertirGrados(valor, conversionGrados_1.grados.F);
     }
 }
